696c6089c5adc0687d3be45ab85270f826699b64
outsung <insung9546@gmail.com>
2021-07-11 18:44:14 +0900

    fix: server url

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -4,7 +4,7 @@ import io, { Socket as SocketType } from 'socket.io-client';
 export default class Socket {
   static instance: null | SocketType = null;
   static init = () => {
-    Socket.instance = io('http://192.168.219.196:5000', {
+    Socket.instance = io('https://interactive-art-t2-server.herokuapp.com', {
       forceNew: true,
       timeout: 5000,
       transports: ['websocket'],
